90daeed16e98db30361099eb14c7b793882bb0a2
Tooni <sayers.neil@gmail.com>
2021-07-06 16:04:44 +0100

    Reimplement product much more efficiently, removing 'trim'

--- lib/mpst/chorautomata.ml
+++ lib/mpst/chorautomata.ml
@@ -67,14 +67,11 @@ let show g =
 type chor_automata_conv_env =
   { g: G.t
   ; tyvars: (TypeVariableName.t * int) list
-  ; alphabet: c_action list
   ; states_to_merge: (int * int) list }
 let init_chor_automata_conv_env:chor_automata_conv_env =
   { g= G.empty
   ; tyvars= []
-  ; alphabet= []
   ; states_to_merge= []}
-
 
 let rec extract_pairs =
   function
@@ -146,7 +143,7 @@ let of_global_type_for_role gty role =
     n
   in
   let rec conv_gtype_aux env = 
-    let {g; tyvars; alphabet; _} = env in
+    let {g; tyvars; _} = env in
     let terminate () =
       if !terminal = ~-1 then
         let curr = fresh () in
@@ -163,8 +160,7 @@ let of_global_type_for_role gty role =
       if RoleName.equal role send_n || RoleName.equal role recv_n then
         let curr = fresh () in
         let a = MsgA (send_n, m, recv_n) in
-        let alphabet = a :: alphabet in
-        let env, next = conv_gtype_aux {env with alphabet} l in
+        let env, next = conv_gtype_aux env l in
         let e = (curr, a, next) in
         let g = env.g in
         let g = G.add_vertex g curr in
@@ -204,10 +200,9 @@ let of_global_type_for_role gty role =
     in
     let env, _ = conv_gtype_aux init_chor_automata_conv_env gty in
     let g = env.g in
-    let alphabet = env.alphabet in
     if not @@ List.is_empty env.states_to_merge then
       let rec aux (start, g) = function
-        | [] -> (alphabet, g)
+        | [] -> g
         | (s1, s2) :: rest ->
             let to_state = Int.min s1 s2 in
             let from_state = Int.max s1 s2 in
@@ -225,122 +220,68 @@ let of_global_type_for_role gty role =
             aux (start, g) rest
       in
       aux (0, g) env.states_to_merge
-    else (alphabet, g)
+    else g
 
-(* https://en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function *)
-let get_cantor_pair (k1, k2) =
-  ((k1 + k2) * (k1 + k2 + 1)) / 2 + k2
-
-let label_equal a1 a2 = 
-  CLabel.compare a1 a2 = 0
-
-(* Find cartesian product of two graphs *)
-(* roughly the 2nd algorithm from http://www.iaeng.org/publication/WCECS2010/WCECS2010_pp141-143.pdf *)
-(* but uses `found_x' and `found_y' to skip some self-transitions *)
-let product (a1, g1) (a2, g2) =
-  if G.is_empty g1 then
-    (a2, g2)
-  else if G.is_empty g2 then
-    (a1, g1)
-  else
-    let count = ref 1 in
-    let fresh () =
-      let n = !count in
-      count := n + 1 ;
-      n
-    in
-    let alphabet = a1 @ a2 in
-    (* cantor pair of (0,0) is 0 *)
-    let g = G.add_vertex G.empty 0 in
-    let r = [(0, 0)] in
-    (* `m' maps cantor pairs to nodes *)
-    (* can't use cantor pairs directly as nodes because they'd get very large in successive products *)
-    let m = Map.add_exn (Map.empty (module Int)) ~key:0 ~data:0 in
-    let rec product_aux (g, r, m) = 
-      if List.is_empty r then
-        (alphabet, g)
-      else
-        let (x, y) as prod_src = List.hd_exn r in
-        let r = List.tl_exn r in
-        product_aux @@ List.fold alphabet ~init:(g, r, m) 
-          ~f:(fun (g, r, m) a ->
-            let find_dest edges src = 
-              match (List.find edges ~f:(fun (_, label, _) -> label_equal label a)) with
-                | Some (_, _, dest) -> (dest, true)
-                | None -> (src, false)
-            in
-            let x_succ = G.succ_e g1 x in
-            let y_succ = G.succ_e g2 y in
-            let (dest_x, found_x) = find_dest x_succ x in
-            let (dest_y, found_y) = find_dest y_succ y in
-            let prod_dest = (dest_x, dest_y) in
-            let prod_dest_encoded = get_cantor_pair prod_dest in
-            let (g, r, m) =
-              if not @@ Map.mem m prod_dest_encoded then
-                let curr = fresh () in
-                (G.add_vertex g curr,
-                 prod_dest :: r,
-                 Map.set m ~key:prod_dest_encoded ~data:curr)
-              else
-                (g, r, m)
-            in
-            if not found_x && not found_y then
-              (g, r, m)
-            else
-              let e_src = Map.find_exn m @@ get_cantor_pair prod_src in
-              let e_dest = Map.find_exn m prod_dest_encoded in
-              let new_e = (e_src, a, e_dest) in
-              (G.add_edge_e g new_e, r, m))
-    in
-    product_aux (g, r, m)
-
-(* `trims' global product to remove transitions/states that break data dependencies. *)
-(* Traverses global product (g) while traversing through equivalent transitions in local graphs (gs). *)
-(* For each global transition, only adds to resultant trimmed graph if transition was 
-   also possible in two local graphs, i.e. the send and the receive *)
-let trim g gs =
-  let ids_to_gs = Map.of_alist_exn (module Int) @@ List.mapi gs ~f:(fun i g -> (i, g)) in
-  let rec trim_aux global_s local_ss result_g seen = 
-    let global_es = G.succ_e g global_s in
-    let local_es = Map.fold local_ss ~init:[] 
-      ~f:(fun ~key:id ~data:s ->
-        let g = Map.find_exn ids_to_gs id in
-        List.cons (id, G.succ_e g s))
-    in
-    List.fold global_es ~init:result_g 
-      ~f:(fun result_g ((_, global_a, dest) as e) ->
-        let matching_locals = List.filter_map local_es 
-          ~f:(fun (id, es) -> 
-            match List.find es ~f:(fun (_, local_a, _) -> label_equal local_a global_a) with
-            | Some (_, _, dest') -> Some (id, dest')
-            | None -> None)
-        in
-        if List.length matching_locals = 2 then
-          let result_g = G.add_edge_e result_g e in
-          let global_s = dest in
-          let local_ss = List.fold matching_locals ~init:local_ss 
-            ~f:(fun local_ss (id, s) -> Map.set local_ss ~key:id ~data:s) 
-          in
-          if List.mem seen global_s ~equal:(=) then 
-            result_g
-          else
-            trim_aux global_s local_ss result_g (dest :: seen)
-        else
-          result_g)
+let product gs =
+  let count = ref 1 in
+  let fresh () =
+    let n = !count in
+    count := n + 1 ;
+    n
   in
-  let global_s = 0 in
-  (* mapping ids to starts *)
-  let local_ss = Map.of_alist_exn (module Int) @@ List.map (Map.keys ids_to_gs) ~f:(fun id -> (id, 0)) in
-  let result_g = G.empty in
-  let seen = [] in
-  trim_aux global_s local_ss result_g seen
+  let system_state_id = 
+    Map.fold
+      ~init:"" 
+      ~f:(fun ~key:id ~data:s acc -> 
+        sprintf "%s@%s.%s" acc (Int.to_string id) (Int.to_string s))
+  in
+  let ids_to_gs = 
+    Map.of_alist_exn (module Int) @@ 
+      List.mapi gs ~f:(fun i g -> (i, g))
+  in
+  let rec product_aux ss prod src m =
+    let es = Map.fold ss ~init:[] 
+      ~f:(fun ~key:id ~data:s acc ->
+        let g = Map.find_exn ids_to_gs id in
+        let next_es = G.succ_e g s in
+        acc @ List.map next_es ~f:(fun e -> (id, e)))
+    in
+    let is_dual ((id1, (_, a1, dest1)), (id2, (_, a2, dest2))) =
+      if CLabel.compare a1 a2 = 0 then
+        Some (a1, (id1, dest1), (id2, dest2))
+      else
+        None
+    in
+    let pairs = extract_pairs es in
+    let duals = List.filter_map pairs ~f:is_dual in
+    if List.is_empty duals then
+      (prod, m)
+    else 
+      List.fold duals ~init:(prod, m)
+        ~f:(fun (prod, m) (a, (id1, dest1), (id2, dest2))  ->
+          let ss = Map.set ss ~key:id1 ~data:dest1 in
+          let ss = Map.set ss ~key:id2 ~data:dest2 in
+          let key = system_state_id ss in
+          if Map.mem m key then 
+            let dest = Map.find_exn m key in 
+            let prod = G.add_edge_e prod (src, a, dest) in
+            (prod, m)
+          else
+            let dest = fresh () in
+            let m = Map.add_exn m ~key ~data:dest in
+            let prod = G.add_edge_e prod (src, a, dest) in
+            product_aux ss prod dest m)
+  in
+  let ss = Map.of_alist_exn (module Int) @@ List.map (Map.keys ids_to_gs) ~f:(fun id -> (id, 0)) in
+  let prod = G.empty in
+  let src = 0 in
+  let m = Map.add_exn (Map.empty (module String)) ~key:(system_state_id ss) ~data:0 in
+  fst @@ product_aux ss prod src m
 
 let of_global_type gty all_roles _(*role*) =
   check_labels gty
   ; let locals = List.map ~f:(of_global_type_for_role gty) all_roles in
   (*List.iter locals ~f:(fun g -> Caml.Format.print_string (show g ^ "\n")) 
   ; Caml.Format.print_string  ("\n----------\n"); *)
-  let (_, naive_product) = List.fold locals ~init:([], G.empty) ~f:product in
-  let trimmed_product = trim naive_product (List.map ~f:snd locals) in
-  trimmed_product
+  product locals
 
